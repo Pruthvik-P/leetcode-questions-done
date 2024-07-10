d36104cc6c1585ea28cab5f119a4200f1e0b824c
Pruthvik_P <Pruthvik6666@gmail.com>
2024-07-10 18:03:53 +0000

    Sync LeetCode submission Runtime - 51 ms (0.25%), Memory - 16.5 MB (100.00%)

---
+++ solutions/1791-richest-customer-wealth/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_wealth = 0
+        current_wealth = 0
+        for i in accounts:
+            current_wealth = sum(i)
+            if current_wealth > max_wealth :
+                max_wealth = current_wealth
+
+        return max_wealth
+        
