fd3141897dc9ffb2049c2cd114b15c72ff6973c5
Pruthvik_P <Pruthvik6666@gmail.com>
2024-07-10 18:01:39 +0000

    Sync LeetCode submission Runtime - 35 ms (0.26%), Memory - 16.5 MB (100.00%)

---
+++ solutions/1833-find-the-highest-altitude/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        max_altitude = 0
+        current_altitude = 0
+        for i in gain:
+            current_altitude += i
+            if current_altitude > max_altitude:
+                max_altitude = current_altitude
+        return max_altitude
