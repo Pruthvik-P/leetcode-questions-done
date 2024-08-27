dad16657d472cb4d5dfd5e6e817d50b7139aa86b
Pruthvik_P <Pruthvik6666@gmail.com>
2024-08-27 08:11:24 +0000

    Sync LeetCode submission Runtime - 40 ms (2.15%), Memory - 16.3 MB (100.00%)

--- solutions/0075-sort-colors/solution.py
+++ solutions/0075-sort-colors/solution.py
@@ -3,4 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums[:]=[0]*nums.count(0)+[1]*nums.count(1)+[2]*nums.count(2)
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+        while mid <= high :
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+
+
