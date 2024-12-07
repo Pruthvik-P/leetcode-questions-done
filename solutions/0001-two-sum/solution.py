60bf47f9f63ce235d834974443abf3413fb52560
Pruthvik_P <Pruthvik6666@gmail.com>
2024-12-07 11:36:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.2 MB (100.00%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,13 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num = sorted(nums)
-        left = 0
-        right = len(nums) - 1 
-        while left < right:
-            if num[left] + num[right] == target:
-                k=[k for k in range(len(nums)) if nums[k] == num[left] or nums[k] == num[right]]
-                return k
-            elif num[left] + num[right] < target:
-                left += 1
-            else:
-                right -= 1        
+        numToIndex = {}
+
+        for i, num in enumerate(nums):
+            if target - num in numToIndex:
+                return numToIndex[target - num], i
+            numToIndex[num] = i
