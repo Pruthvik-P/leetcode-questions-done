7e83c97cfef9e9f08eeed43bf29947139c6a3465
Pruthvik_P <Pruthvik6666@gmail.com>
2025-05-08 18:00:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.3 MB (100.00%)

--- solutions/1677-matrix-diagonal-sum/solution.py
+++ solutions/1677-matrix-diagonal-sum/solution.py
@@ -1,11 +1,13 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        total_sum = 0
         n = len(mat)
+        sum = 0
+
         for i in range(n):
-            total_sum += mat[i][i] + mat[i][n-i-1]
+            sum += mat[i][i] + mat[i][n - i - 1]
+
+        if n % 2 != 0:
+            sum -= mat[n//2][n//2]
         
-        if n % 2 != 0 :
-            total_sum -= mat[n//2][n//2]
-        return total_sum
+        return sum
         
