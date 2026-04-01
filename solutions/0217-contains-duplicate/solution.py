06d6aa26709661eed9b4dca06cc62a545377efe6
Pruthvik_P <Pruthvik6666@gmail.com>
2026-04-01 14:42:49 +0000

    Sync LeetCode submission Runtime - 11 ms (82.47%), Memory - 32.2 MB (54.47%)

--- solutions/0217-contains-duplicate/solution.py
+++ solutions/0217-contains-duplicate/solution.py
@@ -1,11 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         tup = set()
-
         for i in nums:
-            if i in tup:
+            if i  in tup:
                 return True
             tup.add(i)
         return False
-
-
