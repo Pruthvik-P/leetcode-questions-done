adf5d5cbf0ec59cb74ff493dc5c8621e90e52529
Pruthvik_P <Pruthvik6666@gmail.com>
2026-04-06 16:48:09 +0000

    Sync LeetCode submission Runtime - 2 ms (58.41%), Memory - 19.4 MB (31.75%)

---
+++ solutions/1570-final-prices-with-a-special-discount-in-a-shop/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        res = prices[:]
+        stack = []
+
+        for i, price in enumerate(prices):
+            while stack and prices[stack[-1]] >= price:
+                res[stack.pop()] -= price
+
+            stack.append(i)
+
+        return res
