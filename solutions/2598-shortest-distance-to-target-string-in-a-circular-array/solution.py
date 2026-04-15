620e9d0289d884885695728883e8f2ac11433ef9
Pruthvik_P <Pruthvik6666@gmail.com>
2026-04-15 09:32:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (14.72%)

---
+++ solutions/2598-shortest-distance-to-target-string-in-a-circular-array/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        n = len(words)
+
+        for i in range(n):
+            if words[(startIndex + i + n) % n] == target:
+                return i
+            if words[(startIndex - i + n) % n] == target:
+                return i
+        return -1
