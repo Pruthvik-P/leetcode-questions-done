e8d5707a51c43cac4bb33327cd02ee2949582706
Pruthvik_P <Pruthvik6666@gmail.com>
2026-05-20 15:53:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.4 MB (76.82%)

--- solutions/0792-binary-search/solution.py
+++ solutions/0792-binary-search/solution.py
@@ -1,4 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        i = bisect.bisect_left(nums, target)
-        return -1 if i == len(nums) or nums[i] != target else i
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return -1
