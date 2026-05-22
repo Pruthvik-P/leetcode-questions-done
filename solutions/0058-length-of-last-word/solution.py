4b3212a208b9ca17767dbbe2108a6b7764671ada
Pruthvik_P <Pruthvik6666@gmail.com>
2026-05-22 07:28:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (87.55%)

--- solutions/0058-length-of-last-word/solution.py
+++ solutions/0058-length-of-last-word/solution.py
@@ -1,11 +1,3 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        i = len(s) - 1
-
-        while i >= 0 and s[i] == " ":
-            i -= 1
-        last_index = i
-        while i >= 0 and s[i] != " ":
-            i -= 1
-        return last_index - i
-        
+        return len(s.split()[-1]) 
