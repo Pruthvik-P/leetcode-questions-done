5350e1edbe5bc0107fb80ccf898ec2005c8b7d8e
Pruthvik_P <Pruthvik6666@gmail.com>
2026-09-02 15:31:40 +0000

    Sync LeetCode submission Runtime - 51 ms (85.67%), Memory - 29.4 MB (97.74%)

--- solutions/0011-container-with-most-water/solution.py
+++ solutions/0011-container-with-most-water/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        res = 0
         l, r = 0, len(height) - 1
+        res = 0
 
         while l < r:
             area = (r - l) * min(height[l], height[r])
@@ -11,4 +11,5 @@ class Solution:
                 l += 1
             else:
                 r -= 1
+        
         return res
