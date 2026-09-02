6b43332108afde3008112ecc09778d4295370c85
Pruthvik_P <Pruthvik6666@gmail.com>
2026-09-02 10:58:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.7 MB (17.36%)

--- solutions/0026-remove-duplicates-from-sorted-array/solution.py
+++ solutions/0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
-        for j in range(1, len(nums)):
-            if nums[i] != nums[j]:
-                i += 1
-                nums[i] = nums[j]
-
-        return i + 1
+        l = 1
+        
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r - 1]:
+                nums[l] = nums[r]
+                l += 1
+        return l
